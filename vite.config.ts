ef331388bfceaa3c91f17a934e678dceced80ddb
Backooo <cihan.bayat00@gmail.com>
2024-02-06 22:41:03 +0100

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ["**/*.PNG/*.SVG/*.JPG/*.JPEG/*.GIF/*.png/*.svg/*.jpg/*.jpeg/*.gif"],
   base: "https://backooo.github.io/portfolio/",
 })
